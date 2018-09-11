2def6f42c39e85ec47e41a49b64d17098db2973f
Mercado <uidj9286@contiwan.com>
2018-09-10 21:45:13 -0500

    mem_alloc.h file updated

--- task 2 - dynamic memory allocation/SAMV71x/app/getting_started/src/Asw/Mem_Alloc.c
+++ task 2 - dynamic memory allocation/SAMV71x/app/getting_started/src/Asw/Mem_Alloc.c
@@ -22,7 +22,10 @@ void * Mem_Alloc(uint16_t size)
         returnVal = (void*) Mem_AllocControl.current_address;
       /* 
         Allign current address to:
-            8bytes (64bit architecture)*/
+            8bytes (64bit architecture) -> #define TARGET_ARCH ALLIGNMENT_64BIT
+            4bytes (32bit architecture) -> #define TARGET_ARCH ALLIGNMENT_32BIT
+            2bytes (16bit architecture) -> #define TARGET_ARCH ALLIGNMENT_16BIT 
+      */
        currentEndAddress = (currentEndAddress +(TARGET_ARCH-1)) & TARGET_ARCH_MASK;
             Mem_AllocControl.currentEndAddress = (uint8_t*) currentEndAddress;
         printf("Data allocated. \n\r");
---
+++ task 2 - dynamic memory allocation/SAMV71x/app/getting_started/src/Asw/Mem_Alloc.h
@@ -0,0 +1,25 @@
+#ifndef MEM_ALLOC_H
+#define MEM_ALLOC_H
+
+#include "board.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Do not modify */
+#define ALLIGNMENT_64BIT (8)
+#define ALLIGNMENT_32BIT (4)
+#define ALLIGNMENT_16BIT (2)
+#define ALLIGNMENT_8BIT  (1)
+
+#define TARGET_ARCH ALLIGNMENT_32BIT
+
+/* 
+	Fuction: memalloc
+		Allocates
+	Param: size
+		Type is uint16_t: max 65535 bytes size
+*/
+
+extern void *memalloc(uint16_t size);
+
+#endif
