82fb140285d123d3204035b961d6f872f060e08e
Mercado <uidj9286@contiwan.com>
2018-09-10 20:57:31 -0500

    mem_alloc.c file updated.

--- task 2 - dynamic memory allocation/SAMV71x/app/getting_started/src/Asw/Mem_Alloc.c
+++ task 2 - dynamic memory allocation/SAMV71x/app/getting_started/src/Asw/Mem_Alloc.c
@@ -1,11 +1,32 @@
-
 /*
+    Function: Mem_Alloc
+    Allocates
+    Param: size
+    Type is uint16_t: max 65535 bytes size
+*/
+
 void * Mem_Alloc(uint16_t size)
 {
-    uint8_t9 returnVal;
+    uint8_t *returnVal;
     uint32_t currentEndAddress;
 
     currentEndAddress = (uint32_t)Mem_AllocControl.current_address + size;
-    if (current
+    if (currentEndAddress > (uint32_t)Mem_AllocControl.heap_end)
+    {
+      printf("Unable to alocate. Heap End address exceeded.\n\r");
+      returnVal = (void*)0; /* NULL */
+    }
+    else
+    {
+      /* Allocated Pointer Address Update */
+        returnVal = (void*) Mem_AllocControl.current_address;
+      /* 
+        Allign current address to:
+            8bytes (64bit architecture)*/
+       currentEndAddress = (currentEndAddress +(TARGET_ARCH-1)) & TARGET_ARCH_MASK;
+            Mem_AllocControl.currentEndAddress = (uint8_t*) currentEndAddress;
+        printf("Data allocated. \n\r");
+    }
+    return(returnVal);
 }
 */
