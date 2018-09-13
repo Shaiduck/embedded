47d399920247bc2c955a516ae3c8adca8818fbdc
Shaiduck <cristian.shaidt@gmail.com>
2018-09-12 20:09:44 -0500

    Modified main function

--- task 2 - dynamic memory allocation/SAMV71x/app/02_MemAlloc/src/Asw/Main.c
+++ task 2 - dynamic memory allocation/SAMV71x/app/02_MemAlloc/src/Asw/Main.c
@@ -17,28 +17,26 @@
 
 /** Main group of includes for board definitions, chip definitions and type definitions */
 #include    "Std_Types.h"
-/** Task scheduler definitions */
-#include    "SchM.h"
-/** LED control definitions */ 
-#include    "Led_Ctrl.h"
-/** Button control definitions */
-#include    "Button_Ctrl.h"
-/** Watchdog control function prototypes definitions */
-#include    "Wdg.h"
 
-SchMTaskType taskList[SCHM_NUMBER_OF_TASKS] = {
-	{5, TASKS_1_MS, SchM_Task_1ms},
-	{4, TASKS_2_MS_A, SchM_Task_2ms_A},
-	{4, TASKS_2_MS_B, SchM_Task_2ms_B},
-	{3, TASKS_10_MS, SchM_Task_10ms},
-	{2, TASKS_50_MS, SchM_Task_50ms},
-	{1, TASKS_100_MS, SchM_Task_100ms},
-	{5, TASK_SW0, LedCtrl_ToggleLed},
-};
+#include "MemAlloc/Mem_Alloc.h"
 
 /*~~~~~~  Local definitions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 /*~~~~~~  Global variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+typedef struct  DataStruct
+{
+	uint8_t data1;
+	uint16_t data2;
+	uint32_t data3;
+} MyTypeStruct;
+
+#define SIZE_PIR (1024*5) /* 5k size*/
+uint8_t* ptru8;
+uint16_t* ptru16;
+uint32_t ptru32;
+MyTypeStruct* DataStruct;
+uint8_t* ptru8_2;
+uint8_t* ptru8_many_elements;
 
 
 /*~~~~~~  Local functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -53,26 +51,5 @@ SchMTaskType taskList[SCHM_NUMBER_OF_TASKS] = {
  */
 extern int main( void )
 {
-	/* Disable watchdog */
-	Wdg_Disable();
-	printf( "\n\r-- Scheduler Project %s --\n\r", SOFTPACK_VERSION ) ;
-	printf( "-- %s\n\r", BOARD_NAME ) ;
-	printf( "-- Compiled: %s %s With %s --\n\r", __DATE__, __TIME__ , COMPILER_NAME);
-	/* Enable I and D cache */
-	SCB_EnableICache();
-	/* SCB_EnableDCache(); */
-	/* Configure LEDs */
-	printf( "-- Led Control --\n\r" ) ;
-	LedCtrl_Configure(); 
-	printf( "-- Configure Button --\n\r");
-  	ButtonCtrl_ConfigureSW0Button();
-	/* Scheduler Inititalization */
-	printf( "-- Scheduler Initialization --\n\r" ) ;
-	SchM_Init(taskList);
 	
-	/* Should never reach this code */
-	for(;;)
-    {
-		printf( "-- Unexpected Error at Scheduler Initialization --\n\r" ) ;
-	}
 }
