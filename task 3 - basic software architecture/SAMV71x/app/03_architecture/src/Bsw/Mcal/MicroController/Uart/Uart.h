4c9f70b3bfa200b1e0b3d64a58f976a6d0bd0454
shaiduck <cristian.shaidt@gmail.com>
2018-09-24 21:59:22 -0500

    Changes: added definitions.

--- task 3 - basic software architecture/SAMV71x/app/03_architecture/src/Bsw/Mcal/MicroController/Uart/Uart.h
+++ task 3 - basic software architecture/SAMV71x/app/03_architecture/src/Bsw/Mcal/MicroController/Uart/Uart.h
@@ -1,7 +1,28 @@
 #ifndef UART_H        /*prevent duplicated includes*/
 #define UART_H
 
+#include <Std_Types.h>
+
+//placeholder
 typedef int UartConfigType;
-void Uart_init(UartConfigType)
+
+//todo: modify parameters
+void Uart_init(UartConfigType);
+Std_ReturnType Uart_SetBaudRate(uint8_t x, uint32_t y);
+void Uart_SetTxEnable(uint8_t x, uint32_t y);
+void Uart_SetRxEnable(uint8_t x, uint32_t y);
+
+Std_ReturnType Uart_SendByte(uint8_t x, uint8_t y);
+Std_ReturnType Uart_SendBuffer(uint8_t x, uint8_t* y);
+void Uart_GetByte(uint8_t x, uint8_t y);
+
+void Uart_EnableInt(uint8_t x, uint32_t y, uint8_t z);
+void Uart_GetStatus(uint8_t x, uint32_t* y);
+
+void UART0_Handler();
+void UART1_Handler();
+void UART2_Handler();
+void UART3_Handler();
+void UART4_Handler();
 
 #endif
