769d168a1787e731f64fadfbd2df32be2bc91a74
shaiduck <cristian.shaidt@gmail.com>
2018-10-08 21:04:54 -0500

    no mucho

--- task 3 - basic software architecture/SAMV71x/app/08_UartBase/src/Bsw/Mcal/Com/Uart/Uart_Cfg.h
+++ task 3 - basic software architecture/SAMV71x/app/08_UartBase/src/Bsw/Mcal/Com/Uart/Uart_Cfg.h
@@ -21,7 +21,17 @@
   #define UART_CFG_CHANNEL3  (3)
   #define UART_CFG_CHANNEL4  (4)
 
+  #define UART_CFG_INT_DISABLED = 0;
+  #define UART_CFG_INT_RXRDY = 1;
+  #define UART_CFG_INT_TXRDY = 2;
+  #define UART_CFG_INT_OVR_ERROR = 32;
+  #define UART_CFG_INT_FRAME_ERROR = 64;
+  #define UART_CFG_INT_PAR_ERROR = 128;
+  #define UART_CFG_INT_TXEMPTY = 512;
+
   extern const uint8_t UartConfiguredChannels[];
+
+  
 
 
 #endif /* _UART_CFG_H */
