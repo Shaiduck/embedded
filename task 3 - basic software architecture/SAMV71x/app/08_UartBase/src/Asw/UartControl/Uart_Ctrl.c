526a97bca7e2ffe9f7b4450af8eb5c5ccb206748
Shaiduck <cristian.shaidt@gmail.com>
2018-10-14 00:58:11 -0500

    added instructions

--- task 3 - basic software architecture/SAMV71x/app/08_UartBase/src/Asw/UartControl/Uart_Ctrl.c
+++ task 3 - basic software architecture/SAMV71x/app/08_UartBase/src/Asw/UartControl/Uart_Ctrl.c
@@ -17,6 +17,7 @@
 /*****************************************************************************************************
 * Definition of module wide VARIABLEs 
 *****************************************************************************************************/
+uint8_t 		u8SerialCtrl_TxData[] = {"The Atmel_ | SMART_ SAM V71 Xplained Ultra evaluation kit is ideal for evaluating and prototyping with the Atmel SAM V71, SAM V70, SAM S70 and SAM E70 ARM_ Cortex_-M7 based microcontrollers\n\r\n\rExample by Abraham Tezmol\n\r\n\r"};
 
 /****************************************************************************************************
 * Declaration of module wide FUNCTIONs 
@@ -41,34 +42,31 @@
 
 void UartCtrl_2ms( void )
 {
-  /* Example Code, need to be removed */
-  /* Uart Logic Channel 0 */
-  Uart_Send(0);
+// need to add task
+// probably one that enables the interrupt for sending info via uart
+// remember to disable interrupt once inside so that the transmission can be completed
+//without interruptions
 }
 
 void UartCtrl_50ms( void )
 {
-  /* Example Code, need to be removed */
-  /* Uart Logic Channel 1 */
-  Uart_Send(1);
+// need to add task
+// probably one that enables the interrupt for sending info via uart but different channel
 }
 
 void UartCtrl_100ms( void )
 {
-  /* Example Code, need to be removed */
-  /* Uart Logic Channel 2 */
-  Uart_Send(2);
+// need to add task
+// probably one that enables the interrupt for sending info via uart but different channel
 }
 
 void UartCtrl_TriggerEvent( void )
 {
-  /* Example Code, need to be removed */
   /* These function handlers shall be invoked upon interrupt request */
-  UART0_Handler();
-  UART1_Handler();
-  UART2_Handler();
-  UART3_Handler();
-  UART4_Handler();
+  //uart handlers may point to this function
+  //this function then may start sending information
+  //the string that will be send is at the beginning of this file
+  //tasks should be added to scheduler
   /* In your solution this trigger can start a buffer transmission */
 }
 
