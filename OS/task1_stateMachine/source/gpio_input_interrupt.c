6e428d863f105730f623fcf558f8b8211ebc12a8
shaiduck <cristian.shaidt@gmail.com>
2019-03-05 23:03:46 -0600

    added super rough scheduler

--- OS/task1_stateMachine/source/gpio_input_interrupt.c
+++ OS/task1_stateMachine/source/gpio_input_interrupt.c
@@ -13,6 +13,7 @@
 #include "board.h"
 #include "pin_mux.h"
 #include "clock_config.h"
+#include<stdlib.h>
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -47,41 +48,62 @@
 volatile bool g_ButtonPress = false;
 volatile bool g_ButtonPress_2 = false;
 
-void sm_StateA(int param);
-void sm_StateB(int param);
-void sm_StateC(int param);
-void sm_StateD(int param);
+volatile int deletionPending = 0;
+volatile int creationPending = 0;
+#define MAX_TASKS  6
+void sm_StateSuspended(int param);
+void sm_StateCreate(int param);
+void sm_StateDelete(int param);
 void sm_StateE(int param);
 void sm_StateF(int param);
 
 typedef enum
 {
-    STATE_A,
-    STATE_B,
-    STATE_C,
-    STATE_D,
-    STATE_E,
-    STATE_F,
+    STATE_SUSPENDED,
+    STATE_CREATE,
+    STATE_DELETE,
     NUM_STATES
 }StateType;
 
+typedef void (*function_pointer) (int);
 typedef struct
 {
     StateType state;
-    void (*func) (int);
+    function_pointer state_callback;
 }StateMachineType;
 
+typedef struct
+{
+    int numberOfTasks;
+    function_pointer function_array[MAX_TASKS];
+}SchedulerType;
+
+SchedulerType scheduler;
 StateMachineType StateMachine[] = 
 {
-    {STATE_A, sm_StateA},
-    {STATE_B, sm_StateB},
-    {STATE_C, sm_StateC},
-    {STATE_D, sm_StateD},
-    {STATE_E, sm_StateE},
-    {STATE_F, sm_StateF}
+    {STATE_SUSPENDED, sm_StateSuspended},
+    {STATE_CREATE, sm_StateCreate},
+    {STATE_DELETE, sm_StateDelete},
 };
 
-StateType sm_State = STATE_A;
+StateType sm_State = STATE_SUSPENDED;
+
+volatile uint32_t g_systickCounter;
+void SysTick_Handler(void)
+{
+    if (g_systickCounter != 0U)
+    { 
+        g_systickCounter--;
+    }
+}
+void SysTick_DelayTicks(uint32_t n)
+{
+    g_systickCounter = n;
+    while(g_systickCounter != 0U)
+    {
+    }
+}
+
 
 /*******************************************************************************
  * Code
@@ -91,76 +113,82 @@ StateType sm_State = STATE_A;
  *
  * This function toggles the LED
  */
-void sm_StateA(int param)
+void scheduler_task(int taskNumber)
 {
-    if (param == 2)
+	int task = taskNumber +1;
+    PRINTF("\r\nThis is the task number %i and it will last %i seconds\r\n", task, task*5);
+    SysTick_DelayTicks(task * 5000U);
+}
+
+void scheduler_function()
+{
+    int taskAtEnter = scheduler.numberOfTasks;
+    for (int index = 0; index < taskAtEnter; index++)
     {
-        sm_State = STATE_B;
-        PRINTF("A -> B\n");
-    } else if (param == 3)
+        (*scheduler.function_array[index])(index);
+        scheduler.function_array[index] = NULL;
+        scheduler.numberOfTasks--;
+    }
+    if (taskAtEnter != scheduler.numberOfTasks)
     {
-        sm_State = STATE_F;
-        PRINTF("A -> F\n");
+        PRINTF("\r\nNo more tasks\r\n");
     }
 }
-void sm_StateB(int param)
+
+void sm_StateSuspended(int param)
 {
     if (param == 2)
     {
-        sm_State = STATE_A;
-        PRINTF("B -> A\n");
+        sm_State = STATE_CREATE;
     } else if (param == 3)
     {
-        sm_State = STATE_C;
-        PRINTF("B -> C\n");
+        sm_State = STATE_CREATE;
     }
 }
-void sm_StateC(int param)
+void sm_StateCreate(int param)
 {
     if (param == 2)
     {
-        sm_State = STATE_D;
-        PRINTF("C -> D\n");
+        PRINTF("\r\nLet's create a task\r\n");
+        for (int index = 0; index < creationPending; index++)
+        {
+            if (scheduler.numberOfTasks+1 < MAX_TASKS)
+            {
+                scheduler.function_array[scheduler.numberOfTasks] = scheduler_task;
+                scheduler.numberOfTasks++;
+            }
+            else
+            {
+                PRINTF("\r\nMax number of tasks reached\r\n");
+            }
+        }
+        creationPending = 0;
     } else if (param == 3)
     {
-        sm_State = STATE_B;
-        PRINTF("C -> B\n");
+        sm_State = STATE_DELETE;
     }
 }
-void sm_StateD(int param)
+void sm_StateDelete(int param)
 {
     if (param == 2)
     {
-        sm_State = STATE_C;
-        PRINTF("D -> C\n");
+        sm_State = STATE_CREATE;
     } else if (param == 3)
     {
-        sm_State = STATE_E;
-        PRINTF("D -> E\n");
-    }
-}
-void sm_StateE(int param)
-{
-    if (param == 2)
-    {
-        sm_State = STATE_F;
-        PRINTF("E -> F\n");
-    } else if (param == 3)
-    {
-        sm_State = STATE_D;
-        PRINTF("E -> D\n");
-    }
-}
-void sm_StateF(int param)
-{
-    if (param == 2)
-    {
-        sm_State = STATE_E;
-        PRINTF("F -> E\n");
-    } else if (param == 3)
-    {
-        sm_State = STATE_A;
-        PRINTF("F -> A\n");
+        PRINTF("\r\nLet's delete a task\r\n");
+        for (int index = 0; index < deletionPending; index++)
+        {
+            if (scheduler.numberOfTasks-1 >= 0)
+            {
+                scheduler.function_array[scheduler.numberOfTasks] = NULL;
+                scheduler.numberOfTasks--;
+            }
+            else
+            {
+                PRINTF("\r\nNo Tasks to Delete\r\n");
+            }
+        }
+        deletionPending = 0;
     }
 }
 
@@ -170,6 +198,8 @@ void BOARD_SW_IRQ_HANDLER(void)
     GPIO_PortClearInterruptFlags(BOARD_SW_GPIO, 1U << BOARD_SW_GPIO_PIN);
     /* Change state of button. */
     g_ButtonPress = true;
+    creationPending++;
+
 
 
 /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F Store immediate overlapping
@@ -185,7 +215,7 @@ void BOARD_SW_IRQ_HANDLER_2(void)
     GPIO_PortClearInterruptFlags(BOARD_SW_GPIO_2, 1U << BOARD_SW_GPIO_PIN_2);
     /* Change state of button. */
     g_ButtonPress_2 = true;
-
+    deletionPending++;
 /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F Store immediate overlapping
   exception return operation might vector to incorrect interrupt */
 #if defined __CORTEX_M && (__CORTEX_M == 4U)
@@ -233,31 +263,41 @@ int main(void)
     GPIO_PinInit(BOARD_LED_GPIO, BOARD_LED_GPIO_PIN, &led_config);
     GPIO_PinInit(BOARD_LED_GPIO_2, BOARD_LED_GPIO_PIN_2, &led_config);
 
+    if(SysTick_Config(SystemCoreClock / 1000U))
+    {
+        while(1)
+        {
+        }
+    }
+
+
+    scheduler.numberOfTasks = 0;
     while (1)
     {
         if (g_ButtonPress)
         {
-            PRINTF(" %s is pressed \r\n", BOARD_SW_NAME);
+            PRINTF("\r\n %s is pressed \r\n", BOARD_SW_NAME);
             /* Toggle LED. */
             GPIO_PortToggle(BOARD_LED_GPIO, 1U << BOARD_LED_GPIO_PIN);
 
             /* call state machine */
-            (*StateMachine[sm_State].func)(2);
+            (*StateMachine[sm_State].state_callback)(2);
 
             /* Reset state of button. */
             g_ButtonPress = false;
         }
-        if (g_ButtonPress_2)
+        else if (g_ButtonPress_2)
         {
-            PRINTF(" %s is pressed \r\n", BOARD_SW_NAME_2);
+            PRINTF("\r\n %s is pressed \r\n", BOARD_SW_NAME_2);
             /* Toggle LED. */
             GPIO_PortToggle(BOARD_LED_GPIO_2, 1U << BOARD_LED_GPIO_PIN_2);
             
             /* call state machine */
-            (*StateMachine[sm_State].func)(3);
+            (*StateMachine[sm_State].state_callback)(3);
 
             /* Reset state of button. */
             g_ButtonPress_2 = false;
         }
+        scheduler_function();
     }
 }
