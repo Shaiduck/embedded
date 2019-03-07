9fb16bb33490526472a1514f5ae2e49b799148d4
Usuario Labdesi <Usuario@T21816.ds.iteso.mx>
2019-03-06 21:19:26 -0600

    added fifo scheduler with lists

---
+++ OS/task2_FIFOScheduler/source/gpio_input_interrupt.c
@@ -0,0 +1,328 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2018 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "fsl_debug_console.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#include "fsl_common.h"
+#include "board.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define BOARD_LED_GPIO BOARD_LED_RED_GPIO
+#define BOARD_LED_GPIO_PIN BOARD_LED_RED_GPIO_PIN
+
+#define BOARD_SW_GPIO BOARD_SW3_GPIO
+#define BOARD_SW_PORT BOARD_SW3_PORT
+#define BOARD_SW_GPIO_PIN BOARD_SW3_GPIO_PIN
+#define BOARD_SW_IRQ BOARD_SW3_IRQ
+#define BOARD_SW_IRQ_HANDLER BOARD_SW3_IRQ_HANDLER
+#define BOARD_SW_NAME BOARD_SW3_NAME
+
+
+#define BOARD_LED_GPIO_2 BOARD_LED_BLUE_GPIO
+#define BOARD_LED_GPIO_PIN_2 BOARD_LED_BLUE_GPIO_PIN
+
+#define BOARD_SW_GPIO_2 BOARD_SW2_GPIO
+#define BOARD_SW_PORT_2 BOARD_SW2_PORT
+#define BOARD_SW_GPIO_PIN_2 BOARD_SW2_GPIO_PIN
+#define BOARD_SW_IRQ_2 BOARD_SW2_IRQ
+#define BOARD_SW_IRQ_HANDLER_2 BOARD_SW2_IRQ_HANDLER
+#define BOARD_SW_NAME_2 BOARD_SW2_NAME
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+/* Whether the SW button is pressed */
+volatile bool g_ButtonPress = false;
+volatile bool g_ButtonPress_2 = false;
+list_t linkedList;
+
+
+void sm_StateA(int param);
+void sm_StateB(int param);
+void sm_StateC(int param);
+void sm_StateD(int param);
+void sm_StateE(int param);
+void sm_StateF(int param);
+void testCallback();
+
+typedef enum
+{
+    STATE_SUSPENDED,
+    STATE_ADD,
+    STATE_DELETE,
+    STATE_A,
+    STATE_B,
+    STATE_C,
+    STATE_D,
+    STATE_E,
+    STATE_F,
+    NUM_STATES
+}StateType;
+
+typedef struct
+{
+    StateType state;
+    void (*func) (int);
+}StateMachineType;
+
+StateMachineType StateMachine[] = 
+{
+    {STATE_A, sm_StateA},
+    {STATE_B, sm_StateB},
+    {STATE_C, sm_StateC},
+    {STATE_D, sm_StateD},
+    {STATE_E, sm_StateE},
+    {STATE_F, sm_StateF}
+};
+
+StateType sm_State = STATE_A;
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Interrupt service fuction of switch.
+ *
+ * This function toggles the LED
+ */
+void testCallback()
+{
+
+}
+
+void sm_StateSuspended()
+{
+    if (param == 2)
+    {
+        sm_State = STATE_ADD;
+        PRINTF("\r\n initializing\r\n");
+    } else if (param == 3)
+    {
+        sm_State = STATE_DELETE;
+        PRINTF("\r\n initializing\r\n");
+    }
+
+}
+
+void sm_StateAdd()
+{
+    if (param == 2)
+    {
+        PRINTF("\r\n adding task\r\n");
+        list_t newLinkedList;
+        LIST_Init(&newLinkedList, 0, testCallback);
+        LIST_AddTail(&linkedList, LIST_GetHead(&newLinkedList));
+    } else if (param == 3)
+    {
+        sm_State = STATE_DELETE;
+        PRINTF("\r\n deleting task\r\n");
+    }
+}
+
+void sm_StateDelete()
+{
+    if (param == 2)
+    {
+        sm_State = STATE_ADD;
+        PRINTF("\r\n adding task\r\n");
+    } else if (param == 3)
+    {
+        LIST_RemoveHead(&linkedList);
+        PRINTF("\r\n deleting task\r\n");
+        if (LIST_GetSize(&linkedList) == 0)
+        {
+            sm_State = STATE_SUSPENDED;
+        }
+    }
+
+}
+
+void sm_StateA(int param)
+{
+    if (param == 2)
+    {
+        sm_State = STATE_B;
+        PRINTF("A -> B\n");
+    } else if (param == 3)
+    {
+        sm_State = STATE_F;
+        PRINTF("A -> F\n");
+    }
+}
+void sm_StateB(int param)
+{
+    if (param == 2)
+    {
+        sm_State = STATE_A;
+        PRINTF("B -> A\n");
+    } else if (param == 3)
+    {
+        sm_State = STATE_C;
+        PRINTF("B -> C\n");
+    }
+}
+void sm_StateC(int param)
+{
+    if (param == 2)
+    {
+        sm_State = STATE_D;
+        PRINTF("C -> D\n");
+    } else if (param == 3)
+    {
+        sm_State = STATE_B;
+        PRINTF("C -> B\n");
+    }
+}
+void sm_StateD(int param)
+{
+    if (param == 2)
+    {
+        sm_State = STATE_C;
+        PRINTF("D -> C\n");
+    } else if (param == 3)
+    {
+        sm_State = STATE_E;
+        PRINTF("D -> E\n");
+    }
+}
+void sm_StateE(int param)
+{
+    if (param == 2)
+    {
+        sm_State = STATE_F;
+        PRINTF("E -> F\n");
+    } else if (param == 3)
+    {
+        sm_State = STATE_D;
+        PRINTF("E -> D\n");
+    }
+}
+void sm_StateF(int param)
+{
+    if (param == 2)
+    {
+        sm_State = STATE_E;
+        PRINTF("F -> E\n");
+    } else if (param == 3)
+    {
+        sm_State = STATE_A;
+        PRINTF("F -> A\n");
+    }
+}
+
+void BOARD_SW_IRQ_HANDLER(void)
+{
+    /* Clear external interrupt flag. */
+    GPIO_PortClearInterruptFlags(BOARD_SW_GPIO, 1U << BOARD_SW_GPIO_PIN);
+    /* Change state of button. */
+    g_ButtonPress = true;
+
+
+/* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F Store immediate overlapping
+  exception return operation might vector to incorrect interrupt */
+#if defined __CORTEX_M && (__CORTEX_M == 4U)
+    __DSB();
+#endif
+}
+
+void BOARD_SW_IRQ_HANDLER_2(void)
+{
+    /* Clear external interrupt flag. */
+    GPIO_PortClearInterruptFlags(BOARD_SW_GPIO_2, 1U << BOARD_SW_GPIO_PIN_2);
+    /* Change state of button. */
+    g_ButtonPress_2 = true;
+
+/* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F Store immediate overlapping
+  exception return operation might vector to incorrect interrupt */
+#if defined __CORTEX_M && (__CORTEX_M == 4U)
+    __DSB();
+#endif
+}
+
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    /* Define the init structure for the input switch pin */
+    gpio_pin_config_t sw_config = {
+        kGPIO_DigitalInput, 0,
+    };
+
+    /* Define the init structure for the output LED pin */
+    gpio_pin_config_t led_config = {
+        kGPIO_DigitalOutput, 0,
+    };
+
+    BOARD_InitPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    /* Print a note to terminal. */
+    PRINTF("\r\n GPIO Driver example\r\n");
+    PRINTF("\r\n Press %s to turn on/off a LED \r\n", BOARD_SW_NAME);
+
+/* Init input switch GPIO. */
+#if (defined(FSL_FEATURE_PORT_HAS_NO_INTERRUPT) && FSL_FEATURE_PORT_HAS_NO_INTERRUPT)
+    GPIO_SetPinInterruptConfig(BOARD_SW_GPIO_2, BOARD_SW_GPIO_PIN_2, kGPIO_InterruptFallingEdge);
+    GPIO_SetPinInterruptConfig(BOARD_SW_GPIO, BOARD_SW_GPIO_PIN, kGPIO_InterruptFallingEdge);
+#else
+    PORT_SetPinInterruptConfig(BOARD_SW_PORT, BOARD_SW_GPIO_PIN, kPORT_InterruptFallingEdge);
+    PORT_SetPinInterruptConfig(BOARD_SW_PORT_2, BOARD_SW_GPIO_PIN_2, kPORT_InterruptFallingEdge);
+#endif
+    EnableIRQ(BOARD_SW_IRQ);
+    EnableIRQ(BOARD_SW_IRQ_2);
+    GPIO_PinInit(BOARD_SW_GPIO, BOARD_SW_GPIO_PIN, &sw_config);
+    GPIO_PinInit(BOARD_SW_GPIO_2, BOARD_SW_GPIO_PIN_2, &sw_config);
+
+    /* Init output LED GPIO. */
+    GPIO_PinInit(BOARD_LED_GPIO, BOARD_LED_GPIO_PIN, &led_config);
+    GPIO_PinInit(BOARD_LED_GPIO_2, BOARD_LED_GPIO_PIN_2, &led_config);
+
+    LIST_Init(&linkedList, 0, testCallback);
+
+
+    while (1)
+    {
+        if (g_ButtonPress)
+        {
+            PRINTF(" %s is pressed \r\n", BOARD_SW_NAME);
+            /* Toggle LED. */
+            GPIO_PortToggle(BOARD_LED_GPIO, 1U << BOARD_LED_GPIO_PIN);
+
+            /* call state machine */
+            (*StateMachine[sm_State].func)(2);
+
+            /* Reset state of button. */
+            g_ButtonPress = false;
+        }
+        if (g_ButtonPress_2)
+        {
+            PRINTF(" %s is pressed \r\n", BOARD_SW_NAME_2);
+            /* Toggle LED. */
+            GPIO_PortToggle(BOARD_LED_GPIO_2, 1U << BOARD_LED_GPIO_PIN_2);
+            
+            /* call state machine */
+            (*StateMachine[sm_State].func)(3);
+
+            /* Reset state of button. */
+            g_ButtonPress_2 = false;
+        }
+        if (LIST_GetSize(&linkedList) > 0)
+        {
+            //execute tasks
+        }
+    }
+}
